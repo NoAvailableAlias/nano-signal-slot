4bcb842a6c6d3163e374be779cce5e8665842c3e
NoAvailableAlias <apek.blog@gmail.com>
2015-05-22 18:22:49 -0400

    Testing minimal Observer structure for performance. Putting thread safety on hold.

--- nano_observer.hpp
+++ nano_observer.hpp
@@ -3,9 +3,6 @@
 
 #include "nano_function.hpp"
 
-#include <atomic>
-#include <memory>
-
 namespace Nano
 {
 
@@ -14,21 +11,13 @@ class Observer
     template <typename T> friend class Signal;
 
     struct DelegateKeyObserver { DelegateKey delegate; Observer* observer; };
-    struct Node { DelegateKeyObserver data; std::shared_ptr<Node> next; };
-
-    std::shared_ptr<Node> head;
+    struct Node { DelegateKeyObserver data; Node* next; } *head = nullptr;
 
     //--------------------------------------------------------------------------
 
     void insert(DelegateKey const& key, Observer* ptr)
     {
-        auto node = std::make_shared<Node>();
-        node->next = std::atomic_load(&head);
-
-        node->data.delegate = key;
-        node->data.observer = ptr;
-
-        while (!std::atomic_compare_exchange_weak(&head, &node->next, node));
+        head = new Node { { key, ptr }, head };
     }
     void insert(DelegateKey const& key)
     {
@@ -39,10 +28,10 @@ class Observer
 
     void remove(DelegateKey const& key)
     {
-        std::shared_ptr<Node> node = std::atomic_load(&head);
-        std::shared_ptr<Node> prev;
+        Node* node = head;
+        Node* prev = nullptr;
 
-        for ( ; node ; prev = node, node = node->next)
+        for ( ; node; prev = node, node = node->next)
         {
             if (node->data.delegate == key)
             {
@@ -52,9 +41,9 @@ class Observer
                 }
                 else
                 {
-                    while (!std::atomic_compare_exchange_weak(&head, &head, head->next));
+                    head = head->next;
                 }
-                node.reset();
+                delete node;
                 break;
             }
         }
@@ -65,7 +54,7 @@ class Observer
     template <typename Delegate, typename... Uref>
     void onEach(Uref&&... args)
     {
-        for (auto node = std::atomic_load(&head); node; node = node->next)
+        for (auto node = head; node; node = node->next)
         {
             Delegate(node->data.delegate)(std::forward<Uref>(args)...);
         }
@@ -74,14 +63,195 @@ class Observer
     template <typename Delegate, typename Accumulate, typename... Uref>
     void onEach_Accumulate(Accumulate&& accumulator, Uref&&... args)
     {
-        for (auto node = std::atomic_load(&head); node; node = node->next)
+        for (auto node = head; node; node = node->next)
         {
             accumulator(Delegate(node->data.delegate)(std::forward<Uref>(args)...));
+        }
+    }
+
+    //--------------------------------------------------------------------------
+
+    protected:
+
+    ~Observer()
+    {
+        for (auto node = head; node;)
+        {
+            if (node->data.observer == this)
+            {
+                auto temp = node;
+                node = node->next;
+                delete temp;
+            }
+            else
+            {
+                // Remove this slot from this listening Observer
+                node->data.observer->remove(node->data.delegate);
+                node = node->next;
+            }            
         }
     }
 
 };
 
 } // namespace Nano ------------------------------------------------------------
+
+//#include <atomic>
+//#include <memory>
+//
+//namespace Nano
+//{
+//
+//class Observer
+//{
+//    template <typename T> friend class Signal;
+//
+//    struct DelegateKeyObserver
+//    {
+//        DelegateKey delegate;
+//        Observer* observer;
+//    };
+//    struct Node
+//    {
+//        DelegateKeyObserver data;
+//        std::shared_ptr<Node> next;
+//    };
+//
+//    std::shared_ptr<Node> head;
+//
+//    //--------------------------------------------------------------------------
+//
+//    void insert(DelegateKey const& key, Observer* ptr)
+//    {
+//        auto node = std::shared_ptr<Node>(new Node { { key, ptr }, std::atomic_load(&head) }, [](Node* self)
+//        {
+//            self->data.observer->remove(self->data.delegate);
+//            delete self;
+//            self = nullptr;
+//        });
+//
+//        //auto node = std::make_shared<Node>();
+//        //node->next = std::atomic_load(&head);
+//        //node->data.delegate = key;
+//        //node->data.observer = ptr;
+//
+//        while (!std::atomic_compare_exchange_weak(&head, &node->next, node));
+//    }
+//    void insert(DelegateKey const& key)
+//    {
+//        this->insert(key, this);
+//    }
+//
+//    //--------------------------------------------------------------------------
+//
+//    void remove(DelegateKey const& key)
+//    {
+//        std::shared_ptr<Node> node = std::atomic_load(&head);
+//        std::shared_ptr<Node> prev;
+//
+//        for ( ; node ; prev = node, node = node->next)
+//        {
+//            if (node->data.delegate == key)
+//            {
+//                if (prev)
+//                {
+//                    prev->next = node->next;
+//                }
+//                else
+//                {
+//                    while (!std::atomic_compare_exchange_weak(&head, &head, head->next));
+//                }
+//                node.reset();
+//                break;
+//            }
+//        }
+//    }
+//
+//    //--------------------------------------------------------------------------
+//
+//    template <typename Delegate, typename... Uref>
+//    void onEach(Uref&&... args)
+//    {
+//        for (auto node = std::atomic_load(&head); node; node = node->next)
+//        {
+//            Delegate(node->data.delegate)(std::forward<Uref>(args)...);
+//        }
+//    }
+//
+//    template <typename Delegate, typename Accumulate, typename... Uref>
+//    void onEach_Accumulate(Accumulate&& accumulator, Uref&&... args)
+//    {
+//        for (auto node = std::atomic_load(&head); node; node = node->next)
+//        {
+//            accumulator(Delegate(node->data.delegate)(std::forward<Uref>(args)...));
+//        }
+//    }
+//
+//};
+//
+//} // namespace Nano ------------------------------------------------------------
+
+//#include <map>
+//
+//namespace Nano
+//{
+//
+//class Observer
+//{
+//    template <typename T> friend class Signal;
+//
+//    std::map<DelegateKey, Observer*> tracked_connections;
+//
+//    void insert(DelegateKey const& key, Observer* observer)
+//    {
+//        tracked_connections.emplace(key, observer);
+//    }
+//    void insert(DelegateKey const& key)
+//    {
+//        tracked_connections.emplace(key, this);
+//    }
+//    void remove(DelegateKey const& key)
+//    {
+//        tracked_connections.erase(key);
+//    }
+//
+//    template <typename Delegate, typename... Uref>
+//    void onEach(Uref&&... args)
+//    {
+//        for (auto const& slot : tracked_connections)
+//        {
+//            Delegate(slot.first)(std::forward<Uref>(args)...);
+//        }
+//    }
+//    template <typename Delegate, typename Accumulate, typename... Uref>
+//    void onEach_Accumulate(Accumulate&& accumulator, Uref&&... args)
+//    {
+//        for (auto const& slot : tracked_connections)
+//        {
+//            accumulator(Delegate(slot.first)(std::forward<Uref>(args)...));
+//        }
+//    }
+//
+//protected:
+//
+//    ~Observer()
+//    {
+//        auto iter = tracked_connections.cbegin();
+//        auto stop = tracked_connections.cend();
+//
+//        while (iter != stop)
+//        {
+//            auto const& delegate_key = iter->first;
+//            auto const& observer = iter->second;
+//
+//            std::advance(iter, 1);
+//
+//            observer->remove(delegate_key);
+//        }
+//    }
+//};
+//
+//
+//} // namespace Nano ------------------------------------------------------------
 
 #endif // NANO_OBSERVER_HPP
--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -28,51 +28,76 @@ bool handler_d(const char* e, std::size_t n)
     return false;
 }
 
-int main()
+void test_one()
 {
-    Foo foo;
-
     // Declare Nano::Signals using function signature syntax
     Nano::Signal<bool(const char*)> signal_one;
     Nano::Signal<bool(const char*, std::size_t)> signal_two;
 
-    // Connect member functions to Nano::Signals
-    signal_one.connect<Foo, &Foo::handler_a>(&foo);
-    signal_two.connect<Foo, &Foo::handler_b>(&foo);
-
-    // Connect a static member function
-    signal_one.connect<Foo::handler_c>();
-
-    // Connect a free function
-    signal_two.connect<handler_d>();
-
-    // Emit Signals
-    signal_one.emit("we get signal");
-    signal_two.emit("main screen turn on", __LINE__);
-
-    std::vector<bool> status;
-
-    // Emit Signals and accumulate SRVs (signal return values)
-    signal_one.emit_accumulate([&](bool srv)
     {
-        status.push_back(srv);
+        Foo foo;
+
+        // Connect member functions to Nano::Signals
+        signal_one.connect<Foo, &Foo::handler_a>(&foo);
+        signal_two.connect<Foo, &Foo::handler_b>(&foo);
+
+        // Connect a static member function
+        signal_one.connect<Foo::handler_c>();
+
+        // Connect a free function
+        signal_two.connect<handler_d>();
+
+        // Emit Signals
+        signal_one.emit("we get signal");
+        signal_two.emit("main screen turn on", __LINE__);
+
+        std::vector<bool> status;
+
+        // Emit Signals and accumulate SRVs (signal return values)
+        signal_one.emit_accumulate([&](bool srv)
+        {
+            status.push_back(srv);
+        }
+        ,"how are you gentlemen");
+
+        // Disconnect member functions from a Nano::Signal
+        signal_one.disconnect<Foo, &Foo::handler_a>(foo);
+        signal_two.disconnect<Foo, &Foo::handler_b>(foo);
+
+        // Disconnect a static member function
+        signal_one.disconnect<Foo::handler_c>();
+
+        // Disconnect a free function
+        signal_two.disconnect<handler_d>();
+
+        // Emit again to test disconnects
+        signal_one.emit("THIS SHOULD NOT APPEAR");
+        signal_two.emit("THIS SHOULD NOT APPEAR", __LINE__);
+
+        // Test auto disconnect
+        signal_one.connect<Foo, &Foo::handler_a>(foo);
     }
-	,"how are you gentlemen");
-
-    // Disconnect member functions from a Nano::Signal
-    signal_one.disconnect<Foo, &Foo::handler_a>(foo);
-    signal_two.disconnect<Foo, &Foo::handler_b>(foo);
-
-    // Disconnect a static member function
-    signal_one.disconnect<Foo::handler_c>();
-
-    // Disconnect a free function
-    signal_two.disconnect<handler_d>();
-
-    // Emit again to test disconnects
     signal_one.emit("THIS SHOULD NOT APPEAR");
-    signal_two.emit("THIS SHOULD NOT APPEAR", __LINE__);
-
     // Pause the screen
     std::cin.get();
+}
+
+void test_two()
+{
+    using namespace Nano;
+
+    Signal<void()> sig;
+    auto fun = [&](){ std::cout << __LINE__ << std::endl; };
+    sig.connect(&fun);
+    sig.connect(&fun);
+    sig.emit();
+    sig.disconnect(&fun);
+    sig.emit();
+    std::cin.get();
+}
+
+int main()
+{
+    test_one();
+    test_two();
 }
